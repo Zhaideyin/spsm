1989337a95a84f54de8abc6e7b18e63ae9249a55
Zhaideyin <Zhaideyin@xsscd.com>
2016-08-25 15:30:27 +0800

    'u'

--- src/main/java/com/ronxuntech/component/socket/ServerInHandler.java
+++ src/main/java/com/ronxuntech/component/socket/ServerInHandler.java
@@ -1,8 +1,17 @@
 package com.ronxuntech.component.socket;
 
-import java.text.SimpleDateFormat;
+import java.io.File;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 
 import com.ronxuntech.component.socket.util.Fileutil;
 import com.ronxuntech.component.socket.util.IsoChannelMessageFactory;
@@ -16,156 +25,117 @@ import com.ronxuntech.util.SpringBeanFactoryUtils;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-
-
 public class ServerInHandler extends ChannelInboundHandlerAdapter {  
 	//创建service
 	ResponseSocketService responsesocketService = (ResponseSocketService) SpringBeanFactoryUtils.getBean("responsesocketService");
 	RequestSocketService requestsocketService = (RequestSocketService) SpringBeanFactoryUtils.getBean("requestsocketService");
 	SocketPortService socketportService =(SocketPortService) SpringBeanFactoryUtils.getBean("socketportService");
-	
-//	//xml路径。
-	private String filePath;
-//	//有参构造
-	
+	// load up the knowledge base
+	KieServices ks = KieServices.Factory.get();
+	KieContainer kContainer = ks.getKieClasspathContainer();
+	KieSession kSession = kContainer.newKieSession("ksession-rules");
+	Message message = new Message();
+	//xml路径。
+	public String filePath;
 	//无参构造
 	public ServerInHandler() {
 	}
-	
-	//获取xml内容
-//	public String readXml(String filePath){
-//		String content="";
-//		content=Fileutil.readFile(filePath);
-//		return content;
-//	}
-	
+	//有参构造
 	public ServerInHandler(String filePath) {
 		super();
 		this.filePath = filePath;
 	}
-
+    
 	@Override  
-    public void channelRead(ChannelHandlerContext ctx, Object msg)  
-            throws Exception {  
-//		
-//		HttpRequest mReq = (HttpRequest) msg;
-//		String clientIP = mReq.headers().get("X-Forwarded-For");
-//		if (clientIP == null) {
-//			InetSocketAddress insocket = (InetSocketAddress) ctx.channel()
-//					.remoteAddress();
-//			clientIP = insocket.getAddress().getHostAddress();
-//		
-//		}
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {  
 		//获取配置文件的内容。
+		
+		
 		String xmlContent =Fileutil.readFile(getFilePath());
-		System.out.println(xmlContent+"----serverhandler----------------");
-        ByteBuf result = (ByteBuf) msg;  
+		System.out.println("----**************message************----------------");
+		System.out.println("message:"+msg);
+		
+		
+		
+        ByteBuf result = (ByteBuf) msg;
         byte[] result1 = new byte[result.readableBytes()];
+        
+        
         // msg中存储的是ByteBuf类型的数据，把数据读取到byte[]中  
         result.readBytes(result1); 
+        
+    	System.out.println("----**************result1************----------------");
+    	
+    	System.out.println(result1);
+    	
         //将接受的数据转换成16进制
-	    String result2 = new String(result1);
-	    System.out.println("result2:ascii"+result2);
+		//String result2 =  result1.toString();
+    	String result2 = new String(result1);
+		
+	   System.out.println("result2:ascii"+result2.toString());
 	    //转换成16进制
-	    String  resultStr =ParseUtil.stringToHex(result2);
+	    String  resultStr =ParseUtil.stringToHex(result2);	  
+	    
+	    System.out.println("***************转成了16 进制的******************");
 	    System.out.println("resultStr:"+resultStr);
-	   
 		// 接收并打印客户端的信息  
 		// 释放资源，这行很关键  
-//	    System.out.println("Client said:"+result2);  
         result.release();
         PageData pd = new PageData();
       //解析前,先通过端口得到协议，然后再通过协议解析
-        LinkedHashMap<String,Object> map=new LinkedHashMap<>();
         IsoChannelMessageFactory factory=null;
+        LinkedHashMap<String,Object> map =new LinkedHashMap<>();
+        //判断是否出现异常，1表示正常，0表示异常
         int temp=1;
         try{
         	factory=new IsoChannelMessageFactory(getFilePath());
-			map=factory.unpack(resultStr.getBytes(), 0);
-			//接受8583的报文存入数据库
-			if(map!=null){
-				pd.put("STATE","解析成功");
-			}else{
-				pd.put("STATE","解析失败");
-			}
-			System.out.println("****正常执行---------");
-			
+
+        	map =factory.unpack(resultStr.getBytes(), 0);
+        	String pcode=map.get("Bit 3").toString();
+			System.out.println("pcod："+pcode);
+			message.setMessage(resultStr);
+			message.setPath(getFilePath());
+			message.setProcessCode(pcode);
+			message.setBitmap(map);
+			System.out.println("----------------handler-----------"+message.getProcessCode());
+		    kSession.insert(message);
+		    kSession.fireAllRules();
+			  //接受8583的报文存入数据库
+			 pd.put("STATE","1");
         }catch(Exception e){
-        	System.out.println("***catch----------");
-        	 pd.put("STATE","解析失败");
-        	 temp=0;
+        	 pd.put("STATE","0");
+        	 temp=0;//表示异常
         }finally {
         	 pd.put("REQUESTSOCKET_ID", System.currentTimeMillis());
-        	 pd.put("CONTENT", resultStr);
-        	 System.out.println("map is empty:---");
-        	 if(map!=null){
-        		 pd.put("FILEDMAP",map.toString());
-        	 }
+        	 pd.put("CONTENT",resultStr);
+        	 pd.put("FILEDMAP",map.toString());
+
         	 pd.put("CREATETIME",new Date());
         	 requestsocketService.save(pd);
 		}
+            
         
-    	Date date=new Date();
-    	LinkedHashMap<String,Object> data2=null;
-        ByteBuf encoded=null;
-        //如果解析成功。则可以组装，并且存入数据库
-        if(temp==1){
-        	 // 向客户端发送消息  
-    		System.out.println("========================");
-    		if(map!=null){
-    			data2=new LinkedHashMap<String,Object>();
-    			//data2.put("2", map.get("Bit 2"));
-    			data2.put("3", map.get("Bit 3"));
-    			data2.put("4", map.get("Bit 4"));
-    			SimpleDateFormat df0 = new SimpleDateFormat("MMddHHmmss");
-    			SimpleDateFormat df1 = new SimpleDateFormat("HHmmss");
-    			SimpleDateFormat df2 = new SimpleDateFormat("MMdd");
-    			data2.put("7",df0.format(date));
-    			data2.put("12",df1.format(date));
-    			data2.put("13",df2.format(date));
-    			data2.put("15",map.get("Bit 15"));
-    			data2.put("18",map.get("Bit 18"));
-    			data2.put("25",map.get("Bit 25"));
-    			data2.put("32", map.get("Bit 32"));
-    			data2.put("33", map.get("Bit 33"));
-    			data2.put("37", map.get("Bit 37"));
-    			data2.put("41", map.get("Bit 41"));
-    			data2.put("42", map.get("Bit 42"));
-    			data2.put("48", map.get("Bit 48"));
-    			data2.put("49", map.get("Bit 49"));
-    			data2.put("60", map.get("Bit 60"));
-    			data2.put("103", map.get("Bit 103"));
-    		}
-    		//data2.put("128", map.get("Bit 128"));
-    		//System.out.println(new String(factory.pack(data2,"0210")));
-    		String response="";
-    		//返回客户端
-    		if(factory!=null && data2!=null){
-    			response =new String(factory.pack(data2,"0210"));
-    		}
-    		
-            if(response!="" && response!=null){
-            	PageData pd1 = new PageData();
-            	try{
-	            	// 在当前场景下，发送的数据必须转换成ByteBuf数组  
-	                encoded = ctx.alloc().buffer(4 * response.length());  
-	                encoded.writeBytes(response.getBytes()); 
-	                //解析过后组装的报文，传给其他公司的
-	                pd1.put("RESPONSESOCKET_ID",System.currentTimeMillis());
-	                pd1.put("CONTENT", response);
-	                pd1.put("CREATETIME",new Date());
-	                pd1.put("FILEDMAP",data2.toString());
-	                pd1.put("STATE","组装成功");
-            	}catch (Exception e){
-            		pd1.put("STATE","组装失败");
-            	}finally{
-            		responsesocketService.save(pd1);
-            	}
+
+            String response=message.getMessage();
+            LinkedHashMap<String,Object> data2=message.getBitmap();
+            ByteBuf encoded=null;
+            
+            if(response!=" "&& response!=null){
+            	 // 在当前场景下，发送的数据必须转换成ByteBuf数组  
+                encoded = ctx.alloc().buffer(4 * response.length());  
+                encoded.writeBytes(response.getBytes()); 
+                //解析过后组装的报文，传给其他公司的
+                PageData pd1 = new PageData();
+                pd1.put("RESPONSESOCKET_ID",System.currentTimeMillis());
+                pd1.put("CONTENT", response);
+                pd1.put("STATE","1");
+                pd1.put("FILEDMAP",message.getBitmap().toString());
+                pd1.put("CREATETIME",new Date());
+                responsesocketService.save(pd1);
             }
-        }
-        if(encoded!=null  && encoded.toString()!=""){
-        	ctx.write(encoded);  
-        }
+        
+        ctx.write(encoded);  
+
         ctx.flush();  
     }  
   
@@ -179,5 +149,108 @@ public class ServerInHandler extends ChannelInboundHandlerAdapter {
 	public void setFilePath(String filePath) {
 		this.filePath = filePath;
 	}  
+	//报文的内部类
+	public static class Message {
+		private String path;
+		private LinkedHashMap<String, Object> bitmap;
+		private String head;
+		private String returnType;
+		private String processCode;
+		private String message;
+		private int length;
+	
+		public String getType(String  message){
+			byte[] type = new byte[8];
+			System.arraycopy(message.getBytes(), 0, type, 0, 8);
+			return ParseUtil.hexToString(new String(type));
+		}
+		/**
+		 * 
+		 * @param xmlPath xml文件路径
+		 * @param accept 接收到的报文的类型
+		 * @return 返回回应的相应类型
+		 */
+		public String reponseTypeByXml(String xmlPath,String accept)
+		{     String  xmlPath1=getClass().getResource("/").getFile().toString()+xmlPath; 
+			  LinkedHashMap<String ,String> fileds=new  LinkedHashMap<String,String>();
+			  File myXML = new File(xmlPath1);  
+	          SAXReader sr = new SAXReader();
+	          String returnType=null;
+			try {
+				Document  doc = sr.read(myXML);
+				Element root = doc.getRootElement();
+				for (Iterator e = root.elementIterator();e.hasNext();){
+				      Element node = (Element) e.next();
+					  fileds.put(node.elementText("acceptType"), node.elementText("reponseType"));
+				}
+				
+			} catch (DocumentException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}  
+	           if(fileds.containsKey(accept)){
+	        	   returnType =fileds.get(accept);
+	           }
+	           else{
+	        	   System.out.println("erro: no match");
+	           }
+			
+			return returnType;
+		}
+		
+		public String getProcessCode() {
+			return processCode;
+		}
+
+		public void setProcessCode(String processCode) {
+			this.processCode = processCode;
+		}
+
+		public int getLength() {
+			return length;
+		}
+
+		public void setLength(int length) {
+			this.length = length;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
+		public String getReturnType() {
+			return returnType;
+		}
+		public void setReturnType(String returnType) {
+			this.returnType = returnType;
+		}
+
+		public   LinkedHashMap<String, Object> getBitmap() {
+			return bitmap;
+		}
+
+		public void setBitmap(LinkedHashMap<String, Object> bitmap) {
+			this.bitmap = bitmap;
+		}
+
+		public String getHead() {
+			return head;
+		}
+
+		public void setHead(String head) {
+			this.head = head;
+		}
+
+		public String getPath() {
+			return path;
+		}
+
+		public void setPath(String path) {
+			this.path =path;
+		}
+		}
+	}
     
-}  
--- src/main/java/com/ronxuntech/component/socket/util/IsoChannelMessageFactory1.java
+++ src/main/java/com/ronxuntech/component/socket/util/IsoChannelMessageFactory1.java
@@ -1,5 +1,4 @@
-package com.ronxuntech.component.socket.util;
-
+/*package com.ronxuntech.component.socket.util;
 import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
@@ -8,37 +7,30 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
-
 import com.ronxuntech.component.socket.ServerInHandler.Message;
-
-
-/**
+*//**
  * 渠道报文工具
  * BitMap为128位
  * 域值均为ASCII，无压缩
  * @author train
  *
- */
-public class IsoChannelMessageFactory {
-	
+ *//*
+public class IsoChannelMessageFactory1 {
 	private String packet_encoding="UTF-8";
-	
 	private String path;
-	
 	private String TPDU="6009028275";
 	private String HEADER="603100310032";//12位BCD，6字节
 	
-	public IsoChannelMessageFactory(String path){
+	public IsoChannelMessageFactory1(String path){
 //		this.path=getClass().getResource("/").getFile().toString()+path;
 		this.path=path;
 	}
-
-	// 8583报文初始位图:128位01字符串
+	// 8583报文初始位图:128位01字符串,这个位图是写死的
+	
 	public String getInitBitMap128(){
 		String initBitMap = 
 		  "10000000" + "00000000" + "00000000" + "00000000" 
@@ -47,12 +39,93 @@ public class IsoChannelMessageFactory {
 		+ "00000000" + "00000000" + "00000000" + "00000000";
 		return initBitMap;
 	}
-	
-	/**
+	*//**
+	 * 解析报文
+	 * @param bytes
+	 * @param len渠道的头长度为0
+	 * @return
+	 *//*
+	public LinkedHashMap<String,Object> unpack(byte[] bytes,int len){
+		System.out.println(new String(bytes));
+		//bitmap为16字节的ASCII，截取后转换成16进制
+		int bitMapLength=32;
+		//重新格式化为ASCII(4字节)类型+16进制(16字节)位图+ASCII+16进制(8字节)
+		byte[] type = new byte[8];
+		byte[] bitMap = new byte[bitMapLength];
+		byte[] body=new byte[bytes.length-8-bitMapLength-16];
+		byte[] mac=new byte[16];//加密字符
+		System.arraycopy(bytes, 0, type, 0,8);
+		System.arraycopy(bytes, 8, bitMap, 0,bitMapLength);
+		System.arraycopy(bytes, 8+bitMapLength, body, 0,bytes.length-8-bitMapLength-16);
+		System.arraycopy(bytes, bytes.length-16, mac, 0,16);
+		//重新组装bytes
+		bytes=StringUtil.arrayApend(ParseUtil.hexToString(new String(type)).getBytes(),bitMap);
+		//body转换成ascii
+		bytes=StringUtil.arrayApend(bytes,ParseUtil.hexToString(new String(body)).getBytes());
+		bytes=StringUtil.arrayApend(bytes,mac);
+
+		LinkedHashMap<String,Object> filedMap=new LinkedHashMap<String,Object>();
+		try {
+			//截掉头长度
+			System.arraycopy(bytes, len+4, bitMap, 0, bitMapLength);
+			//转换成16进制后再转换为2进制
+			String bitMapStr=ParseUtil.bcdhex2binary(new String(bitMap));
+			//记录当前位置,从位图后开始遍历取值 
+			int pos = len+4+bitMapLength;
+	        for (int i = 0; i < bitMapStr.length(); i++) {
+	        	String filedValue = "";//字段值
+	        	String filedName = "Bit "+(i+1);//修改为整型，原来为String
+	        	
+		        if (i!=0&&bitMapStr.charAt(i) == '1') {
+		        	//获取域定义信息
+		        	Map<String,Object> field=getField(i+1);
+		        	String defType=field.get("type").toString();//类型定义例string
+					int defLen=(int) field.get("length");//长度定义,例20
+					boolean isFixLen=true;//是否定长判断
+
+					
+					if(defType.indexOf(".")>0){//变长域
+						isFixLen=false;
+						defLen=StringUtil.countCharAtString(defType,".".charAt(0));//获取类型后面.的个数
+					}
+					// 截取该域信息
+					if (!isFixLen) {//变长域
+						int defLen1 =defLen;
+                        
+						String realLenStr=new String(bytes, pos, defLen1, packet_encoding);//报文中实际记录域长,例如16,023
+						
+						int realLen=Integer.valueOf(realLenStr);
+					
+						int realAllLen=defLen1+realLen;//该字段总长度（包括长度值占的长度）
+						byte[] filedValueByte=new byte[Integer.valueOf(realLen)];
+						System.arraycopy(bytes, pos+defLen1, filedValueByte, 0, filedValueByte.length);
+						filedValue=new String(filedValueByte,packet_encoding);
+						pos += realAllLen;//记录当前位置
+
+					} else {//定长域
+						int defLen2 = defLen;
+						if(defType.startsWith("ascii")||defType.equals("b")){
+							defLen2=defLen2*2;
+						    }
+						filedValue = new String(bytes, pos, defLen2, packet_encoding);
+						pos += defLen2;//记录当前位置
+					}
+					System.out.println("***************域——————值***************");
+					System.out.println(filedName+" ，"+filedValue);
+					filedMap.put(filedName, filedValue);
+		        	
+		        }
+	        }
+        }catch (Exception e) {
+			e.printStackTrace();
+		}
+		return filedMap;
+	}
+	*//**
 	 * 根据交易码的不同返回，需要组装的域
 	 * @param 根据交易码的不同返回的域的相应配置文件名xmlPath
 	 * @return 放回相应的map
-	 */
+	 *//*
 	public  LinkedHashMap<Integer,Object> getReponseFiled(String xmlPath)
 	{     String  xmlPath1=getClass().getResource("/").getFile().toString()+xmlPath; 
 		  LinkedHashMap<Integer,Object> fileds=new  LinkedHashMap<Integer,Object>();
@@ -75,17 +148,17 @@ public class IsoChannelMessageFactory {
 		return fileds;
 		
 	}
-	/**
+	*//**
 	 * 
 	 * @param datafiledMap 解析后得到的map
 	 * @param xmlPath 配置文件 例如190000xml
 	 * @param m 获得返回的类型
 	 * @return
-	 */
+	 *//*
 	
 	public byte[] packNew(LinkedHashMap<String,Object> datafiledMap,String xmlPath, Message m){
 		LinkedHashMap<Integer,Object> field=getReponseFiled(xmlPath);
-		Map<String,Object> map=formatValueTo8583(datafiledMap,this.getInitBitMap128());
+		Map<String,Object> map=formatValueTo8583(datafiledMap);
 		LinkedHashMap<String,Object> formatedFiledMap=(LinkedHashMap<String, Object>) map.get("formatedFiledMap");
 		LinkedHashMap<String,Object> returnMap=new LinkedHashMap<String,Object>();
 		System.out.println("*******解析后得到的域********"+formatedFiledMap.toString());
@@ -118,114 +191,24 @@ public class IsoChannelMessageFactory {
 		 System.out.println("返回的报文*************"+new String(returnBytes));
 		return returnBytes;
 	}
-
-	/**
-	 * 解析报文
-	 * @param bytes
-	 * @param len 头长度
-	 * @return
-	 */
-	public LinkedHashMap<String,Object> unpack(byte[] bytes,int len){
-		try{
-			
-		//bitmap为16字节的ASCII，截取后转换成16进制
-		int bitMapLength=32;
-		//重新格式化为ASCII(4字节)+16进制(16字节)+ASCII+16进制(8字节)
-		byte[] type = new byte[8];
-		byte[] bitMap = new byte[bitMapLength];
-		byte[] body=new byte[bytes.length-8-bitMapLength-16];
-		byte[] mac=new byte[16];
-		System.arraycopy(bytes, 0, type, 0,8);
-		System.arraycopy(bytes, 8, bitMap, 0,bitMapLength);
-		System.arraycopy(bytes, 8+bitMapLength, body, 0,bytes.length-8-bitMapLength-16);
-		System.arraycopy(bytes, bytes.length-16, mac, 0,16);
-		bytes=StringUtil.arrayApend(ParseUtil.hexToString(new String(type)).getBytes(),bitMap);
-		//body转换成ascii
-		bytes=StringUtil.arrayApend(bytes,ParseUtil.hexToString(new String(body)).getBytes());
-		bytes=StringUtil.arrayApend(bytes,mac);
-
-		LinkedHashMap<String,Object> filedMap=new LinkedHashMap<String,Object>();
-		try {
-			
-			//截掉头长度
-			System.arraycopy(bytes, len+4, bitMap, 0, bitMapLength);
-			//转换成16进制后再转换为2进制
-			String bitMapStr=ParseUtil.bcdhex2binary(new String(bitMap));
-			//记录当前位置,从位图后开始遍历取值 
-			int pos = len+4+bitMapLength;
-	        for (int i = 0; i < bitMapStr.length(); i++) {
-	        	String filedValue = "";//字段值
-	        	String filedName ="Bit "+(i+1);
-//	        	System.out.println("filedname:"+filedName);
-		        if (i!=0&&bitMapStr.charAt(i) == '1') {
-		        	//获取域定义信息
-		        	Map<String,Object> field=getField(i+1);
-		        	System.out.println("field:"+field.get("id"));
-		        	String defType=field.get("type").toString();//类型定义例string
-					int defLen=(int) field.get("length");//长度定义,例20
-					boolean isFixLen=true;//是否定长判断
-
-					
-					if(defType.indexOf(".")>0){//变长域
-						isFixLen=false;
-						defLen=StringUtil.countCharAtString(defType,".".charAt(0));//获取类型后面.的个数
-					}
-					// 截取该域信息
-					if (!isFixLen) {//变长域
-						int defLen1 =defLen;
-
-						String realLenStr=new String(bytes, pos, defLen1, packet_encoding);//报文中实际记录域长,例如16,023
-						
-						int realLen=Integer.valueOf(realLenStr);
-					
-						int realAllLen=defLen1+realLen;//该字段总长度（包括长度值占的长度）
-
-						byte[] filedValueByte=new byte[Integer.valueOf(realLen)];
-						System.arraycopy(bytes, pos+defLen1, filedValueByte, 0, filedValueByte.length);
-						filedValue=new String(filedValueByte,packet_encoding);
-						pos += realAllLen;//记录当前位置
-
-					} else {//定长域
-						int defLen2 = defLen;
-						if(defType.startsWith("ascii")||defType.equals("b")){//startsWith 测试直字符串否有指定前缀
-							defLen2=defLen2*2;
-						}
-						filedValue = new String(bytes, pos, defLen2, packet_encoding);
-						pos += defLen2;//记录当前位置
-					}
-					System.out.println("******"+filedName+",,, "+ filedValue);
-				
-					filedMap.put(filedName, filedValue);
-		        	
-		        }
-	        }
-        }catch (Exception e) {
-			e.printStackTrace();
-		}
-		return filedMap;
-}catch(Exception e){
-			System.out.println("ex");
-		}
-		return null;
-	}
-	/**
+	*//**
 	 * 组装报文
-	 * @param datafiledMap
+	 * @param datafiledMap 解析后得到的报文的域的map
+	 * @param message 报文,
 	 * @return
-	 */
-	public byte[] pack(LinkedHashMap<String,Object> datafiledMap,Message m){
-		 String reponseType=m.getReturnType();
-		Map<String,Object> all=formatValueTo8583(datafiledMap,this.getInitBitMap128());
+	 *//*
+	public byte[] pack(LinkedHashMap<String,Object> datafiledMap, Message m){
+	   String reponseType=m.getReturnType();
+	   Map<String,Object> all=formatValueTo8583(datafiledMap);
 		try {
 			String  bitMap=(String)all.get("bitMap");
 			// 128域位图二进制字符串转16位16进制
 			byte[] bitmaps= ParseUtil.binary2hex(bitMap).getBytes();
-			
 			LinkedHashMap pacBody=(LinkedHashMap)all.get("formatedFiledMap");
 			StringBuffer last128=new StringBuffer();
 			Iterator it=pacBody.keySet().iterator();
 			for(;it.hasNext();){
-				String key=(String)it.next();
+				Integer key=(Integer)it.next();
 				String value=(String)pacBody.get(key);
 				last128.append(value);
 			}
@@ -252,17 +235,25 @@ public class IsoChannelMessageFactory {
 		}
 		return null;
 	}
-	public Map<String,Object> formatValueTo8583(LinkedHashMap<String,Object> filedMap,String  bitMap){
+	
+	*//**
+	 * 
+	 * 
+	 * @param datafiledMap 解析后得到的域的map
+	 * @param bitMap 位图
+	 * @return
+	 *//*
+	public Map<String,Object> formatValueTo8583(LinkedHashMap<String, Object> datafiledMap){
+		String bitMap=this.getInitBitMap128();
 		Map<String,Object> map=new HashMap<String,Object>();
 		LinkedHashMap<String,Object> formatedFiledMap=new LinkedHashMap<String,Object>();
-		if(filedMap!=null){
-			Iterator it=filedMap.keySet().iterator();
+		if(datafiledMap!=null){
+			Iterator it=datafiledMap.keySet().iterator();
 			for(;it.hasNext();){
-				String fieldId=it.next().toString().substring(4);
-				String fieldValue=(String)filedMap.get(fieldId);
-				
+				String fieldId=(String)it.next();
+				String fieldValue=(String)datafiledMap.get(fieldId);
 				try{
-					if (fieldValue == null) {
+					if (fieldValue == null){
 						System.out.println("error:报文域 {" + fieldId + "}为空值");
 						fieldValue = "";
 						return null;
@@ -270,13 +261,13 @@ public class IsoChannelMessageFactory {
 					//将域值编码转换，保证报文编码统一
 					fieldValue=new String(fieldValue.getBytes(packet_encoding),packet_encoding);
 					// 组二进制位图串
-					bitMap = changeBitMapFlag(fieldId, bitMap);
+				//	bitMap = changeBitMapFlag(fieldId, bitMap);
 					//获取域定义信息
-		        	Map<String,Object> field=getField(Integer.parseInt(fieldId));
+		        	//Map<String,Object> field=getField(fieldId);
 		        	String defType=field.get("type").toString();//类型定义例string
 					int defLen=(int) field.get("length");//长度定义,例20
-					boolean isFixLen=true;//是否定长判断
-		
+					boolean isFixLen=true;
+					//是否定长判断
 					if(defType.indexOf(".")>0){//变长域
 						isFixLen=false;
 						defLen=StringUtil.countCharAtString(defType,".".charAt(0));//获取类型后面.的个数
@@ -327,7 +318,7 @@ public class IsoChannelMessageFactory {
 					}
 
 					// 返回结果赋值
-					if (filedMap.containsKey(fieldId)) {
+					if (datafiledMap.containsKey(fieldId)) {
 						if (formatedFiledMap.containsKey(fieldId)) {
 							formatedFiledMap.remove(fieldId);
 						}
@@ -346,8 +337,14 @@ public class IsoChannelMessageFactory {
 		map.put("bitMap", bitMap);
 		return map;
 	}
+	
+	*//**
+	 * 根据传进来的ID号获取相应的map类
+	 * @param id
+	 * @return map
+	 *//*
 	public Map<String,Object> getField(int id){
-		 File myXML = new File(path);  
+		 File myXML = new File(path); 
          SAXReader sr = new SAXReader();  
          try {  
             Document doc  =  sr.read(myXML);  
@@ -363,23 +360,41 @@ public class IsoChannelMessageFactory {
                 }
             }
             return null;
-        } catch (DocumentException e) {  
+        } catch (DocumentException e){
             e.printStackTrace();  
         }
          return null;
 	}
-	public static String changeBitMapFlag(String fieldNo, String res) {
-		int indexNo=Integer.parseInt(fieldNo);
-		res = res.substring(0, indexNo-1) + "1" + res.substring(indexNo);
+	*//**
+	 * 用来获取报文的交易码
+	 * @param message传过来的报文
+	 * @return 该段报文的交易码
+	 *//*
+	public String getProcessCode(String message){
+		byte[] bytes=message.getBytes();
+		LinkedHashMap<String,Object> lkhmap= unpack(bytes,0);
+		String processCode=(String)lkhmap.get(3);
+		return processCode;
+	}
+	
+	*//**
+	 * 根据域是否存在，组装bitmap.如果域存在则，相应的位为1，否则相应的为0
+	 * @param fieldNo
+	 * @param res
+	 * @return
+	 *//*
+	public static String changeBitMapFlag(Integer fieldNo, String res) {
+		//int indexNo=Integer.parseInt(fieldNo);
+		res = res.substring(0,fieldNo-1) + "1" + res.substring(fieldNo);
 		return res;
 	}
-	/**
+	*//**
 	 * 获取字符串变长值
 	 * @param valueStr
 	 * @param defLen
 	 * 例如getFixLengthValue("12345678", 2)返回08
 	 * 例如getFixLengthValue("12345678", 3)返回0008
-	**/
+	**//*
 	public String getVaryLengthValue(String valueStr,int defLen){
 		return getVaryLengthValue(valueStr,defLen,packet_encoding);
 	}
@@ -408,12 +423,12 @@ public class IsoChannelMessageFactory {
 		return fixLen;
 	}
 	
-	/**
+	*//**
 	 * 将字段值做定长处理，不足定长则在后面补空格
 	 * @param valueStr
 	 * @param defLen
 	 * @return
-	 */
+	 *//*
 	public String getFixFieldValue(String valueStr,int defLen){
 		return getFixFieldValue(valueStr,defLen,packet_encoding);
 	}
@@ -452,3 +467,4 @@ public class IsoChannelMessageFactory {
 		return sb.toString();
 	}
 }
+*/
